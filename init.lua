7a90d2f8d8771279d2d5f042203da897c72a93dc
Levi Yan <leviyanx@gmail.com>
2025-05-20 20:21:14 +0800

    new(theme): 应用新主题catppuccin

--- init.lua
+++ init.lua
@@ -4,9 +4,10 @@ require('autocmd')
 
 -- 安装插件
 require('plugins')
--- （主题）插件
-require('plugin-config/tokyonight')
-require('plugin-config/theme-vscode')
+-- 启用主题
+-- require('plugin-config/tokyonight')
+-- require('plugin-config/theme-vscode')
+require('plugin-config/catppuccin')
 -- 启用插件
 require('plugin-config/nvimtree')
 require('plugin-config/lualine')
@@ -14,6 +15,3 @@ require('plugin-config/whichkey')
 require('plugin-config/toggleterm')
 require('plugin-config/telescope')
 require('plugin-config/treesitter')
-
--- 启用主题
-vim.cmd[[colorscheme vscode]]
