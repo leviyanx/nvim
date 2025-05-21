4f4d658a09996ff56e3a41eeda0e902b804ea4d2
Levi Yan <leviyanx@gmail.com>
2025-05-21 11:14:01 +0800

    rename keymaps.lua to keymappings.lua

--- lua/lsp/lua.lua
+++ lua/lsp/lua.lua
@@ -1,4 +1,4 @@
-local lsp_set_keymap = require("keymaps")
+local lsp_set_keymap = require("keymappings")
 -- 定义快捷键
 -- 根据官方的提示，这里我们使用 on_attach 表示当前缓冲加载服务端完成之后调用
 local on_attach = function(_, bufnr)
