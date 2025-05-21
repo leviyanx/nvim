72deb8bb19f9fde31133df8f265811096c12018d
Levi Yan <leviyanx@gmail.com>
2025-05-21 17:22:12 +0800

    edit(cmp): 自动补全增加tab/S-tab键选择

--- lua/plugin-config/cmp.lua
+++ lua/plugin-config/cmp.lua
@@ -14,8 +14,10 @@ cmp.setup({
     mapping = {
         -- 选择上一个
         ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
         -- 选择下一个
         ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
         -- 出现补全
         ['<A-.>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
         -- 取消补全
