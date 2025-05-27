16b67c72b8be7e17f0199cb27a4e667bec017b0a
Levi Yan <leviyanx@gmail.com>
2025-05-27 21:54:56 +0800

    edit(gitsigns): 跳转hunk的快捷键改为gp, gn

--- lua/plugin-config/gitsigns.lua
+++ lua/plugin-config/gitsigns.lua
@@ -35,8 +35,8 @@ M.opts = {
 
     -- 快捷键
     on_attach = function()
-        vim.keymap.set('n', 'gj', '<cmd>Gitsigns next_hunk<CR>', {silent = true, desc = '跳转到下一个修改'})
-        vim.keymap.set('n', 'gk', '<cmd>Gitsigns prev_hunk<CR>', {silent = true, desc = '跳转到上一个修改'})
+        vim.keymap.set('n', 'gp', '<cmd>Gitsigns prev_hunk<CR>', {silent = true, desc = '跳转到上一个修改'})
+        vim.keymap.set('n', 'gn', '<cmd>Gitsigns next_hunk<CR>', {silent = true, desc = '跳转到下一个修改'})
         vim.keymap.set({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', {silent = true, desc = '暂存修改'})
         vim.keymap.set({'n', 'v'}, '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>', {silent = true, desc = '撤销暂存的修改'})
         vim.keymap.set('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>', {silent = true, desc = '预览修改'})
