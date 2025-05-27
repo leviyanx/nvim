6121f0b7c884683dc240540c2da2df02423fa4ed
Levi Yan <leviyanx@gmail.com>
2025-05-27 22:43:32 +0800

    new(ftplugin): 增加typescript的缩进规定

---
+++ ftplugin/typescript.lua
@@ -0,0 +1,5 @@
+-- 设置tab转换为space
+vim.bo.shiftwidth = 2    -- 空格数，用于缩进
+vim.bo.tabstop = 2       -- 一个制表符代表的空格数
+vim.bo.softtabstop = 2   -- 当按下 Tab 键时，插入的空格数
+vim.bo.expandtab = true  -- 将 Tab 转换为空格
