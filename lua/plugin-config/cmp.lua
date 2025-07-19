36d66f3c029d3acfbc1914e035325c4dada83540
Levi Yan <leviyanx@gmail.com>
2025-07-19 18:51:58 +0800

    edit(cmp): ctrl-n, ctrl-p作为vsnip snippet的jump快捷键

--- lua/plugin-config/cmp.lua
+++ lua/plugin-config/cmp.lua
@@ -1,6 +1,17 @@
 local M = {}
 
 function M.setup()
+
+    local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
+    local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+    end
+
     local cmp = require("cmp")
     local lspkind = require("lspkind")
     cmp.setup({
@@ -16,10 +27,26 @@ function M.setup()
 
         mapping = {
             -- 选择上一个
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
+            ['<C-p>'] = cmp.mapping(function()
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                    feedkey("<Plug>(vsnip-jump-prev)", "")
+                end
+            end, { "i", "s" }),
             ['<Up>'] = cmp.mapping.select_prev_item(),
             -- 选择下一个
-            ['<C-n>'] = cmp.mapping.select_next_item(),
+            ['<C-n>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif vim.fn["vsnip#available"](1) == 1 then
+                    feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                elseif has_words_before() then
+                    cmp.complete()
+                else
+                    fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                end
+            end, { "i", "s" }),
             ['<Down>'] = cmp.mapping.select_next_item(),
             -- 出现补全
             ['<A-.>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
